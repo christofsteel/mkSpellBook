0aafc6f81d73897997107ff027aed1b689b8c388
Christoph Stahl <christoph.stahl@uni-dortmund.de>
2013-02-06 22:24:37 +0100

    added -o option for output, added chaptermarks for spelllevel

--- mkspellbook.py
+++ mkspellbook.py
@@ -17,6 +17,12 @@ def replace(dictionary, string):
 def condition(dictionary, string):
     return conditionregexp.sub(lambda m : replace(dictionary, m.group("string")) if m.group("condition") in dictionary and dictionary[m.group("condition")] else "", string)
 
+def write(output, string):
+    if output:
+        output.write(string)
+    else:
+        print(string)
+
 def texify(string):
     replacements = {
         "<li>": r"\\item ",
@@ -45,6 +51,7 @@ def texify(string):
     
 parser = argparse.ArgumentParser(description="Creates a Spellbook")
 parser.add_argument("--template", "-t", default="plain")
+parser.add_argument("--output", "-o", type=argparse.FileType('w'), metavar="FILE")
 args = parser.parse_args()
 templatepath = "templates/" + args.template + "/"
 
@@ -56,12 +63,17 @@ con.row_factory = sqlite3.Row
 spellcur = con.cursor()
 descriptorcur = con.cursor()
 
-spellcur.execute("select distinct * from spells join levels on spells.id = levels.spell order by level,name;")
+spellcur.execute("select * from spells join levels on spells.id = levels.spell where class = 'Cleric' and edition = 'Core (3.5)' order by level,name;")
 
-head = open('head.tex', 'r')
-print(head.read())
+head = open(templatepath + 'head.tex', 'r')
+write(args.output, head.read())
 row = spellcur.fetchone()
+currlevel = row['level']
+write(args.output, "\\chapter{Level " + str(currlevel) + "}")
 while row is not None:
+    if currlevel != row['level']:
+        currlevel = row['level']
+        write(args.output, "\\chapter{Level " + str(currlevel) + "}")
     dictrow = dict(row)
     descriptorcur.execute("select descriptor from descriptors where spell = " + str(row['id']))
     drow = descriptorcur.fetchone()
@@ -71,8 +83,11 @@ while row is not None:
         drow = descriptorcur.fetchone()
     cond = condition(dictrow, templatestring)
     replaced = replace(dictrow, cond)
-    print(replaced)
+    write(args.output, replaced)
     row = spellcur.fetchone()
-tail = open('tail.tex', 'r')
-print(tail.read())
+tail = open(templatepath + 'tail.tex', 'r')
+write(args.output, tail.read())
+
+if args.output:
+    args.output.close()
 
