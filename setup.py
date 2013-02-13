2095e2091c49013cb92de81a61df17ce62cde579
Christoph Stahl <christoph.stahl@uni-dortmund.de>
2013-02-14 00:03:43 +0100

    further rearranging

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(name="mkSpellbook",
+	version = "0.2",
+	description = "Generates a Dungeons & Dragons 3rd Edition (D&D 3.0 and D&D 3.5) spellbook based on the spells available on dndtools.eu",
+	author = "Christoph \"Hammy\" Stahl",
+	author_email = "christoph.stahl@uni-dortmund.de",
+	url = "https://github.com/christofsteel/mkspellbook",
+	packages=['mkspellbook']
