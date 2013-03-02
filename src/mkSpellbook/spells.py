3e4663981b72256d966036765ae7d61475c1afa7
Christoph Stahl <christoph.stahl@uni-dortmund.de>
2013-03-02 21:11:05 +0100

    spelllisting now works for sqlite

--- src/mkSpellbook/spells.py
+++ src/mkSpellbook/spells.py
@@ -1,13 +1,10 @@
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 from mkSpellbook.models import *
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm.exc import NoResultFound
 
 
 class Spells:
 	def __init__(self, database):
-		engine = create_engine(database)
+		engine = create_engine("sqlite:///" + database)
 		Session = sessionmaker(bind=engine)
 		self.session = Session()		
 		Base.metadata.create_all(engine)
@@ -16,41 +13,45 @@ class Spells:
 		return [spell.ruleset for spell in self.session.query(Spell.ruleset).distinct()]
 
 	def listClasses(self, ruleset):
-		query = self.session.query(ClassLevel.d20class)
-		query = query.filter(ClassLevel.spells.any(Spell.ruleset == ruleset))
-		return [cl.d20class for cl in query.distinct()]
+		query = self.session.query(ClassLevel.d20class).filter(ClassLevel.spells.any(Spell.ruleset == ruleset)).distinct().all()
+		return [cl.d20class for cl in query]
 
 	def listBooks(self, ruleset, d20class):
-		query = self.ession.query(Spell.book, Spell.edition)
+		query = self.session.query(Spell.book, Spell.edition)
 		query = query.filter(Spell.ruleset == ruleset)
 		query = query.filter(Spell.classlevels.any(ClassLevel.d20class == d20class))
-		return query.distinct().all()
+		return query.distinct().order_by(Spell.edition, Spell.book).all()
 
 	def listLevels(self, ruleset, d20class, book):
 		query = self.session.query(ClassLevel.level)
-		query = query.filter(ClassLevel.spells.any(Spell.ruleset == ruleset, Spell.book == book))
+		query = query.filter(ClassLevel.spells.any(Spell.ruleset == ruleset)).filter(ClassLevel.spells.any(Spell.book ==  book))
 		query = query.filter(ClassLevel.d20class == d20class)
 		return [cl.level for cl in query.distinct()]
 
 	def listSpells(self, ruleset, d20class, book, level):
-		query = self.session.query(Spells)
+		query = self.session.query(Spell)
 		query = query.filter(Spell.ruleset == ruleset, Spell.book == book)
-		query = query.filter(Spell.classlevels.any(ClassLevel.d20class == d20class, ClassLevel.level == level))
+		query = query.filter(Spell.s2cl.classlevels.any(d20class = d20class, level = level))
 		return query.all()
 	
-	def mkDescriptors(self, descriptors):
-		return [self.session.query(Descriptor).filter(Descriptor.descriptor == descriptor).first() or Descriptor(descriptor) for descriptor in descriptors]
+	def listSpellsWithClasslevels(self, ruleset, d20class, book, level):
+		query = self.session.query(SpellWithClasslevel)
+		query = query.filter(SpellWithClasslevel.spell.has(Spell.ruleset==ruleset), SpellWithClasslevel.spell.has(Spell.book == book))
+		query = query.filter(SpellWithClasslevel.classlevel.has(ClassLevel.d20class == d20class), SpellWithClasslevel.classlevel.has(ClassLevel.level == level))
+		return query.all()
 
-	def mkClassLevel(self, classlevel):
-		return self.session.query(ClassLevel).filter(ClassLevel.d20class == classlevel.d20class, ClassLevel.level == classlevel.level).first() or ClassLevel(classlevel.d20class, classlevel.level)
+	def listSpellbooks(self):
+		return self.session.query(Spellbook).all()
+
+	def getSpellbook(self, spellbook):
+		return self.session.query(Spellbook).filter(Spellbook.name == spellbook).first() or Spellbook(name = spellbook)
+	
+	def getDescriptor(self, descriptor):
+		return self.session.query(Descriptor).filter(Descriptor.descriptor == descriptor).first() or Descriptor(descriptor)
+
+	def getClassLevel(self, d20class, level):
+		return self.session.query(ClassLevel).filter(ClassLevel.d20class == d20class, ClassLevel.level == level).first() or ClassLevel(d20class, level)
 
 	def addSpell(self, spell):
-		spell.descriptors = self.mkDescriptors([descriptor.descriptor for descriptor in spell.descriptors])
-		spell.classlevels = [self.mkClassLevel(cl) for cl in spell.classlevels]
 		self.session.add(spell)
-#		self.session.flush()
-#		self.session.commit()
-
-
-
-
+		self.session.commit()
