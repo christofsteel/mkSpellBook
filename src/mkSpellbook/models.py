4b1dec060a446103455ab0ad44f0971cf10bfa4f
Christoph Stahl <christoph.stahl@uni-dortmund.de>
2013-03-02 21:10:26 +0100

    Added spellbook to sqlite

--- src/mkSpellbook/models.py
+++ src/mkSpellbook/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Boolean, LargeBinary, Integer, String, create_engine, ForeignKey, Table, UniqueConstraint
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
 
 Base = declarative_base()
 
@@ -13,17 +14,17 @@ classlevels_to_spells = Table("classlevels_to_spells", Base.metadata,
 	Column('spell_id', Integer, ForeignKey('spells.id')),
 	Column('classlevel_id', Integer, ForeignKey('classlevels.id')))
 
-spellbooks_to_spellinspellbook = Table("spellbooks_to_spellinspellbook", Base.metadata,
+spellbooks_to_spellswithclasslevels = Table("spellbooks_to_spellswithclasslevels", Base.metadata,
 	Column("spellbooks_id", Integer, ForeignKey("spellbooks.id")),
-	Column("spellsinspellbook_id", Integer, ForeignKey("spell_in_spellbook.id")))
+	Column("spellswithclasslevels_id", Integer, ForeignKey("spells_with_classlevels.id")))
 
-class SpellInSpellbook(Base):
-	__tablename__ = "spell_in_spellbook"
+class SpellWithClasslevel(Base):
+	__tablename__ = "spells_with_classlevels"
 	id = Column(Integer, primary_key = True)
 	spell_id = Column(Integer, ForeignKey('spells.id'))
-	spell = relationship("Spell")
 	classlevel_id = Column(Integer, ForeignKey('classlevels.id'))
-	classlevel = relationship("ClassLevel")
+	classlevel = relationship("ClassLevel", backref="cl2s")
+	spell = relationship("Spell", backref="s2cl")
 
 
 class Descriptor(Base):
@@ -32,10 +33,6 @@ class Descriptor(Base):
 	descriptor = Column(String, unique=True)
 	def __init__(self, descriptor):
 		self.descriptor = descriptor
-"""	def __eq__(self, other):
-		return isinstance(other, Descriptor) and \
-				other.descriptor == self.descriptor
-"""
 
 class Spell(Base):
 	__tablename__ = "spells"
@@ -62,32 +59,28 @@ class Spell(Base):
 	spellres = Column(String)
 	spelltext = Column(String)
 	descriptors = relationship("Descriptor", secondary=descriptors_to_spells, backref="spells")
-	classlevels = relationship("ClassLevel", secondary=classlevels_to_spells, backref="spells")
+	classlevels = association_proxy("s2cl", "classlevel")
+	
 
 class ClassLevel(Base):
 	__tablename__ = "classlevels"
 	id = Column(Integer, primary_key = True)
 	d20class = Column(String)
 	level = Column(Integer)
+	spells = association_proxy("cl2s", "spell")
 	__table_args__ = (UniqueConstraint(d20class, level), )
 	def __init__(self, d20class, level):
 		self.d20class = d20class
 		self.level = level
 	
-"""	def __eq__(self, other):
-		return isinstance(other, ClassLevel) and \
-				other.d20class == self.d20class and \
-				other.lebel == self.level
-"""
-
 class Spellbook(Base):
 	__tablename__ = "spellbooks"
 	id = Column(Integer, primary_key = True)
 	author = Column(String)
-	name = Column(String)
+	name = Column(String, unique = True)
 	author = Column(String)
 	logo = Column(LargeBinary)
 	logoext = Column(String)
-	spells = relationship("SpellInSpellbook", secondary=spellbooks_to_spellinspellbook)
+	spells = relationship("SpellWithClasslevel", secondary=spellbooks_to_spellswithclasslevels)
 
 
