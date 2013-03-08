d6b3d79ae537f822515cae192ba6e329268e16a1
Christoph Stahl <christoph.stahl@uni-dortmund.de>
2013-03-08 22:25:09 +0100

    add script asks for spelltext

--- src/mkSpellbook/importscripts/script_add_manually.py
+++ src/mkSpellbook/importscripts/script_add_manually.py
@@ -29,30 +29,32 @@ def runimport(database):
 			("Link","",50)]
 	frc, fch = d.form("Add Spells", fields=fields)
 	if fch and not frc:
-		choices = fch.split("\n")
-		s = Spell()
-		s.name = choices[0]
-		s.ruleset = choices[1]
-		s.edition = choices[2]
-		s.book = choices[3]
-		s.s2cl = [SpellWithClasslevel(spell=s, classlevel=spells.getClassLevel(cl[0], int(cl[1]))) for cl in [classlevel.split() for classlevel in choices[4].split(",")]]
-		s.school = choices[5]
-		s.subschool = choices[6]
-		s.descriptors = [spells.getDescriptor(desc.strip()) for desc in choices[7].split(",")]
-		s.verbal = choices[8] and choices[7] != "0"
-		s.somatic = choices[9] and choices[8] != "0"
-		s.material = choices[10] and choices[9] != "0"
-		s.arcanefocus = choices[11] and choices[10] != "0"
-		s.divinefocus = choices[12] and choices[11] != "0"
-		s.xpcost = choices[13] and choices[12] != "0"
-		s.castingtime = choices[14]
-		s.spellrange = choices[15]
-		s.area = choices[16]
-		s.target = choices[17]
-		s.duration = choices[18]
-		s.savingthrow = choices[19]
-		s.spellres = choices[20]
-		s.link = choices[21]
-		s.spelltext = "Foobar"
-		spells.session.add(s)
-		spells.session.commit()
+		erc, spelltext = d.editbox()
+		if erc:
+			choices = fch.split("\n")
+			s = Spell()
+			s.name = choices[0]
+			s.ruleset = choices[1]
+			s.edition = choices[2]
+			s.book = choices[3]
+			s.s2cl = [SpellWithClasslevel(spell=s, classlevel=spells.getClassLevel(cl[0], int(cl[1]))) for cl in [classlevel.split() for classlevel in choices[4].split(",")]]
+			s.school = choices[5]
+			s.subschool = choices[6]
+			s.descriptors = [spells.getDescriptor(desc.strip()) for desc in choices[7].split(",")]
+			s.verbal = choices[8] and choices[7] != "0"
+			s.somatic = choices[9] and choices[8] != "0"
+			s.material = choices[10] and choices[9] != "0"
+			s.arcanefocus = choices[11] and choices[10] != "0"
+			s.divinefocus = choices[12] and choices[11] != "0"
+			s.xpcost = choices[13] and choices[12] != "0"
+			s.castingtime = choices[14]
+			s.spellrange = choices[15]
+			s.area = choices[16]
+			s.target = choices[17]
+			s.duration = choices[18]
+			s.savingthrow = choices[19]
+			s.spellres = choices[20]
+			s.link = choices[21]
+			s.spelltext = spelltext
+			spells.session.add(s)
+			spells.session.commit()
